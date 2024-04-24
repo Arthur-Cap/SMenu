df5398353014fac12e82c35bf4113ccdfcfccb8f
Arthur-Cap <thanhdat.ctd2003@gmail.com>
2024-04-24 13:31:24 +0700

    Commit before bring to remote git

--- src/main/java/com/SMenu/SMenu/controller/UserController.java
+++ src/main/java/com/SMenu/SMenu/controller/UserController.java
@@ -31,7 +31,6 @@ public class UserController {
     public ResponseEntity<List<User>> allUsers() {
         List <User> users = userService.allUsers();
 
-
         return ResponseEntity.ok(users);
     }
 }

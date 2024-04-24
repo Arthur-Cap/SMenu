462bdbe0fa877e4a4428e3cd4303f1bed4df7b32
Arthur-Cap <thanhdat.ctd2003@gmail.com>
2024-04-24 14:33:52 +0700

    Commit before bring to remote git

--- src/test/java/com/SMenu/SMenu/SMenuApplicationTests.java
+++ src/test/java/com/SMenu/SMenu/SMenuApplicationTests.java
@@ -1,14 +1,19 @@
 package com.SMenu.SMenu;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class SMenuApplicationTests {
 
 	@Test
 	void huhu() {
-
+		assertEquals(10, 10);
 	}
 
 }
